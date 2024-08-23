36503c705334ca1d8c77177806ab1e4349861b09
David Kerr <kerrky@gmail.com>
2024-08-23 16:13:16 -0400

    Initial Commit

---
+++ app/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+from sqlalchemy import MetaData
+from config import Config
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+
+
+# Instantiate REST API
+api = Api(app)
+
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": "*",
+            "methods": [
+                "GET",
+                "POST",
+                "PUT",
+                "PATCH",
+                "DELETE",
+                "OPTIONS",
+            ],
+            "allow_headers": [
+                "Content-Type",
+                "Authorization",
+            ],
+        }
+    },
+)
+
+from app import models, routes
